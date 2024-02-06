580abd826680693c8d977fa06d7ee6fccae60253
zzhou292 <38740063+zzhou292@users.noreply.github.com>
2024-02-06 01:35:48 -0600

    add back folder

---
+++ Walnut/Walnut/src/Walnut/EntryPoint.h
@@ -0,0 +1,20 @@
+#pragma once
+
+extern Walnut::Application *Walnut::CreateApplication(int argc, char **argv);
+bool g_ApplicationRunning = true;
+
+namespace Walnut {
+
+int Main(int argc, char **argv) {
+  while (g_ApplicationRunning) {
+    Walnut::Application *app = Walnut::CreateApplication(argc, argv);
+    app->Run();
+    delete app;
+  }
+
+  return 0;
+}
+
+} // namespace Walnut
+
+int main(int argc, char **argv) { return Walnut::Main(argc, argv); }
