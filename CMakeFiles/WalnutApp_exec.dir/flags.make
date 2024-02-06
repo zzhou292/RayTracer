e9e5f73288ba7d2c6070caf57714b22e695f5fee
zzhou292 <38740063+zzhou292@users.noreply.github.com>
2024-02-06 01:07:44 -0600

    initial random noise generation

--- CMakeFiles/WalnutApp_exec.dir/flags.make
+++ CMakeFiles/WalnutApp_exec.dir/flags.make
@@ -6,7 +6,7 @@ CXX_DEFINES =
 
 CXX_INCLUDES = -I/opt/homebrew/include -I/Users/jasonzhou/VulkanSDK/1.3.275.0/macOS/include -I/Users/jasonzhou/Desktop/STUDY/RayTracer/./Walnut/vendor/imgui -I/Users/jasonzhou/Desktop/STUDY/RayTracer/./Walnut/vendor/stb_image -I/Users/jasonzhou/Desktop/STUDY/RayTracer/./Walnut/Walnut/src
 
-CXX_FLAGSarm64 =  -fno-autolink -std=gnu++17 -arch arm64 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX13.3.sdk
+CXX_FLAGSarm64 =  -fno-autolink -O3 -DNDEBUG -std=gnu++17 -arch arm64 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX13.3.sdk
 
-CXX_FLAGS =  -fno-autolink -std=gnu++17 -arch arm64 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX13.3.sdk
+CXX_FLAGS =  -fno-autolink -O3 -DNDEBUG -std=gnu++17 -arch arm64 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX13.3.sdk
 
--- WalnutApp/src/WalnutApp.cpp
+++ WalnutApp/src/WalnutApp.cpp
@@ -2,37 +2,80 @@
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Image.h"
+#include "Walnut/Random.h"
+#include "Walnut/Timer.h"
 
-class ExampleLayer : public Walnut::Layer
-{
+using namespace Walnut;
+
+class ExampleLayer : public Walnut::Layer {
 public:
-	virtual void OnUIRender() override
-	{
-		ImGui::Begin("Hello");
-		ImGui::Button("Button");
-		ImGui::End();
+  virtual void OnUIRender() override {
+    ImGui::Begin("Settings");
+    ImGui::Text("Last Render Time: %.2fms", m_LastRenderTime);
+    if (ImGui::Button("Render")) {
+      Render();
+    };
+    ImGui::End();
 
-		ImGui::ShowDemoWindow();
-	}
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+    ImGui::Begin("Viewport");
+
+    m_ViewportWidth = ImGui::GetContentRegionAvail().x;
+    m_ViewportHeight = ImGui::GetContentRegionAvail().y;
+
+    if (m_Image)
+      ImGui::Image(m_Image->GetDescriptorSet(),
+                   {(float)m_Image->GetWidth(), (float)m_Image->GetHeight()});
+
+    ImGui::End();
+    ImGui::PopStyleVar();
+
+    Render();
+  }
+
+  void Render() {
+    Timer timer;
+
+    if (!m_Image || m_ViewportWidth != m_Image->GetWidth() ||
+        m_ViewportHeight != m_Image->GetHeight()) {
+      m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight,
+                                        ImageFormat::RGBA);
+      delete[] m_ImageData;
+      m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
+    }
+
+    for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++) {
+      m_ImageData[i] = Random::UInt();
+      m_ImageData[i] |= 0xff000000;
+    }
+
+    m_Image->SetData(m_ImageData);
+
+    m_LastRenderTime = timer.ElapsedMillis();
+  }
+
+private:
+  std::shared_ptr<Image> m_Image;
+  uint32_t *m_ImageData = nullptr;
+  uint32_t m_ViewportWidth = 0;
+  uint32_t m_ViewportHeight = 0;
+
+  float m_LastRenderTime = 0.0f;
 };
 
-Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
-{
-	Walnut::ApplicationSpecification spec;
-	spec.Name = "Walnut Example";
+Walnut::Application *Walnut::CreateApplication(int argc, char **argv) {
+  Walnut::ApplicationSpecification spec;
+  spec.Name = "Json Ray Tracer";
 
-	Walnut::Application* app = new Walnut::Application(spec);
-	app->PushLayer<ExampleLayer>();
-	app->SetMenubarCallback([app]()
-	{
-		if (ImGui::BeginMenu("File"))
-		{
-			if (ImGui::MenuItem("Exit"))
-			{
-				app->Close();
-			}
-			ImGui::EndMenu();
-		}
-	});
-	return app;
+  Walnut::Application *app = new Walnut::Application(spec);
+  app->PushLayer<ExampleLayer>();
+  app->SetMenubarCallback([app]() {
+    if (ImGui::BeginMenu("File")) {
+      if (ImGui::MenuItem("Exit")) {
+        app->Close();
+      }
+      ImGui::EndMenu();
+    }
+  });
+  return app;
 }
